463ad07248798410fe8b3332a90e74647c05e084
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-21 14:40:44 -0800

    Employee wage UC5 wages for a month done

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,7 +2,7 @@ class EmpWageComputation{
       static final int IS_FULL_TIME=1;
       static final int IS_PART_TIME=2;
       static final int EMP_RATE_PER_HOUR=20;
-
+		static final int NUM_OF_WORKING_DAYS=20;
 	public static void main(String args[])
 	{
 		int empHrs=0;
@@ -11,14 +11,14 @@ class EmpWageComputation{
 		switch (empCheck){
 		case IS_FULL_TIME:
 				empHrs=8;
-				System.out.println("Employee is present");
-				empWage=empHrs*EMP_RATE_PER_HOUR;
+				System.out.println("Employee is FullTime");
+				empWage=empHrs*EMP_RATE_PER_HOUR*NUM_OF_WORKING_DAYS;
 				System.out.println("Emp Wage : " + empWage);
 				break;
 		case IS_PART_TIME:
             empHrs=4;
             System.out.println("Employee is PartTime");
-            empWage=empHrs*EMP_RATE_PER_HOUR;
+            empWage=empHrs*EMP_RATE_PER_HOUR*NUM_OF_WORKING_DAYS;
             System.out.println("Emp Wage : " + empWage);
          	break;
 		default:
