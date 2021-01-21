dbb63d081c858d582287bb78eb3d8ad44fabf18a
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-20 22:03:16 -0800

    Calculating Employee Wage UC 2 done

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,6 +1,22 @@
 class EmpWageComputation{
 	public static void main(String args[])
 	{
-		System.out.println("Welcome to Employee wage computaton master branch");
+		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
+		int empHrs=0;
+		int empWage=0;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck == IS_FULL_TIME)
+			{
+				empHrs=8;
+				System.out.println("Employee is present");
+				empWage=empHrs*EMP_RATE_PER_HOUR;
+				System.out.println("Emp Wage : " + empWage);
+			}
+			else
+			{
+				System.out.println("Employee is absent");
+				System.out.println("Employee wage is 0");
+			}
 	}
 }
