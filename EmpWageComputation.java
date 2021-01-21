74782b53c2ac338c60990ac8fd84529a0d14864d
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-21 15:30:11 -0800

    Total Working hours and days in a condition part done

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,29 +1,34 @@
-class EmpWageComputation{
+class EmpWageComputation
+{
       static final int IS_FULL_TIME=1;
       static final int IS_PART_TIME=2;
       static final int EMP_RATE_PER_HOUR=20;
-		static final int NUM_OF_WORKING_DAYS=20;
-	public static void main(String args[])
-	{
-		int empHrs=0;
-		int empWage=0;
-		int empCheck=(int) Math.floor(Math.random()*10)%3;
-		switch (empCheck){
-		case IS_FULL_TIME:
-				empHrs=8;
-				System.out.println("Employee is FullTime");
-				empWage=empHrs*EMP_RATE_PER_HOUR*NUM_OF_WORKING_DAYS;
-				System.out.println("Emp Wage : " + empWage);
-				break;
-		case IS_PART_TIME:
-            empHrs=4;
-            System.out.println("Employee is PartTime");
-            empWage=empHrs*EMP_RATE_PER_HOUR*NUM_OF_WORKING_DAYS;
-            System.out.println("Emp Wage : " + empWage);
-         	break;
-		default:
-				System.out.println("Employee is absent");
-				System.out.println("Employee wage is 0");
-		}
-	}
+      static final int NUM_OF_WORKING_DAYS=20;
+      static final int MAX_HOURS_IN_A_MONTH=100;
+   public static void main(String args[])
+   {
+      int empHrs=0;
+      int totalEmpHours=0;
+      int totalWorkingDays=0;
+      while(totalEmpHours<=MAX_HOURS_IN_A_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
+      {
+         totalWorkingDays++;
+         int empCheck=(int) Math.floor(Math.random()*10)%3;
+         switch (empCheck)
+         {
+            case IS_FULL_TIME:
+               empHrs=8;
+               break;
+            case IS_PART_TIME:
+               empHrs=4;
+               break;
+            default:
+               empHrs=0;
+         }
+         totalEmpHours+=empHrs;
+         System.out.println("Day : " +totalWorkingDays + "     "+ "Emp hours : " +empHrs);
+      }
+      int totalEmpWage=totalEmpHours*EMP_RATE_PER_HOUR;
+      System.out.println("Total Emp Wage : " +totalEmpWage);
+   }
 }
