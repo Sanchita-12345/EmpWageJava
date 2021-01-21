0fe9f31a6f2e998dda9ad3033a3b567f4ce27b94
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-21 10:59:39 -0800

    Employee wage Builder UC3 is done

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,10 +2,30 @@ class EmpWageComputation{
 	public static void main(String args[])
 	{
 		int IS_FULL_TIME=1;
-		double empCheck=Math.floor(Math.random()*10)%2;
+		int IS_PART_TIME=2;
+		int EMP_RATE_PER_HOUR=20;
+		int empHrs=0;
+		int empWage=0;
+		double empCheck=Math.floor(Math.random()*10)%3;
 		if(empCheck == IS_FULL_TIME)
-			System.out.println("Employee is present");
-		else
-			System.out.println("Employee is absent");
+			{
+				empHrs=8;
+				System.out.println("Employee is FullTime");
+				empWage=empHrs*EMP_RATE_PER_HOUR;
+				System.out.println("Emp Wage : " + empWage);
+			}
+			else if(empCheck == IS_PART_TIME)
+         {
+            empHrs=4;
+            System.out.println("Employee is PartTime");
+            empWage=empHrs*EMP_RATE_PER_HOUR;
+            System.out.println("Emp Wage : " + empWage);
+         }
+
+			else
+			{
+				System.out.println("Employee is absent");
+				System.out.println("Employee wage is 0");
+			}
 	}
 }
